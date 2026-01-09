a73a11f7a92557e1e40bdf6f302e2f8558c457b1
cipherodio <cipherodio@gmail.com>
2026-01-10 00:24:09 +0800

    Lsp server configuration

---
+++ .config/nvim/lsp/yamlls.lua
@@ -0,0 +1,19 @@
+return {
+    cmd = { 'yaml-language-server', '--stdio' },
+    filetypes = {
+        "yaml",
+        "yaml.docker-compose",
+        "yaml.gitlab",
+        "yaml.helm-values",
+    },
+    root_markers = { '.git' },
+    settings = {
+        redhat = { telemetry = { enabled = false } },
+        yaml = { format = { enable = true } },
+    },
+    on_init = function (client)
+        client.server_capabilities.documentFormattingProvider = true
+    end,
+}
+
+-- Last Modified: Tue, 30 Dec 2025 12:43:39 PM
