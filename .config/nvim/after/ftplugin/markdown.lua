a848de853931cc4f06e9f98397a856af944a1b1c
cipherodio <cipherodio@gmail.com>
2026-01-10 00:44:29 +0800

    Refactor

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,73 +1,23 @@
-local query = vim.treesitter.query.parse("markdown", "((atx_heading) @header)")
-local map = vim.keymap.set
+local km = require("utils.keymaps")
 local opt = vim.opt_local
-local g = vim.g
 
-opt.foldmethod = "expr"
-opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-opt.spell = true
-opt.conceallevel = 0
 -- opt.conceallevel = 2
-opt.concealcursor = ""
-opt.list = false
-g.vim_markdown_conceal_code_blocks = 0
+-- opt.concealcursor = ""
+opt.spell = true
+opt.textwidth = 72
+opt.linebreak = true
 
-g.markdown_fenced_languages = {
-    "sh",
-    "zsh",
-    "bash",
-    "lua",
-    "html",
-    "vim",
-}
+km.nmap("<leader>tc", function()
+    local cl = vim.api.nvim_get_option_value("conceallevel", { scope = "local" })
 
-map("n", "<Tab>", function()
-    if vim.bo.filetype == "markdown" then
-        local current_fold = vim.fn.foldclosed(".")
-        if current_fold == -1 then
-            vim.cmd("normal! zc")
-        else
-            vim.cmd("normal! zo")
-        end
+    if cl == 0 then
+        vim.api.nvim_set_option_value("conceallevel", 2, { scope = "local" })
+        vim.api.nvim_set_option_value("concealcursor", "", { scope = "local" })
+    else
+        vim.api.nvim_set_option_value("conceallevel", 0, { scope = "local" })
     end
-end, { desc = "Fold toggle markdown files" })
+end, { desc = "Toggle conceal" })
 
-map({ "n", "i" }, "<C-Space>", function()
-    local line = vim.api.nvim_get_current_line()
-    local modified_line = line:gsub(
-        "([-%*+] %[)(.)(%])",
-        function(prefix, checkbox, postfix)
-            checkbox = (checkbox == " ") and "x" or " "
-            return prefix .. checkbox .. postfix
-        end
-    )
-    vim.api.nvim_set_current_line(modified_line)
-end, { desc = "Toggle checkboxes", buffer = true })
+-- km.nmap("<Tab>", "za", { desc = "Toggle fold (markdown only)" })
 
-map({ "n", "i" }, "<C-;>", function()
-    local root = vim.treesitter.get_parser():parse()[1]:root()
-    local _, node, _ = query:iter_captures(root, 0, vim.fn.line("."), -1)()
-    if not node then
-        return
-    end
-    require("nvim-treesitter.ts_utils").goto_node(node)
-    vim.cmd("normal zt")
-end, { silent = true, buffer = 0, desc = "Next markdown header" })
-
-map({ "n", "i" }, "<C-'>", function()
-    local root = vim.treesitter.get_parser():parse()[1]:root()
-    if vim.fn.line(".") == 1 then
-        return
-    end
-    local node
-    for _, n, _ in query:iter_captures(root, 0, 0, vim.fn.line(".") - 1) do
-        node = n
-    end
-    if not node then
-        return
-    end
-    require("nvim-treesitter.ts_utils").goto_node(node)
-    vim.cmd("normal zt")
-end, { silent = true, buffer = 0, desc = "Previous markdown header" })
-
--- Last Modified: Wed, 05 Feb 2025 02:57:23 PM
+-- Last Modified: Wed, 07 Jan 2026 02:44:14 AM
