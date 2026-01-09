86cc215effd9aef6c4067e9669b1b630f618318f
cipherodio <cipherodio@gmail.com>
2026-01-10 00:38:41 +0800

    Better handling of installing parsers

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,55 +1,112 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-        ensure_installed = {
-            "bash",
-            "commonlisp",
-            "cpp",
-            "css",
-            "diff",
-            "gitcommit",
-            "gitignore",
-            "git_config",
-            "git_rebase",
-            "html",
-            "ini",
-            "javascript",
-            "jsdoc",
-            "json",
-            "lua",
-            "luadoc",
-            "luap",
-            "markdown",
-            "markdown_inline",
-            "printf",
-            "python",
-            "query",
-            "rasi",
-            "ssh_config",
-            "tmux",
-            "toml",
-            "typescript",
-            "vim",
-            "vimdoc",
-            "xml",
-            "yaml",
-            "zathurarc",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        lazy = false,
+        event = { "BufReadPre", "BufNewFile" },
+        build = ":TSUpdate",
+
+        dependencies = {
+            "andymass/vim-matchup",
         },
-        highlight = { enable = true },
-        indent = { enable = true },
-        auto_install = true,
-        matchup = {
-            enable = true,
-            include_match_words = true,
-            enable_quotes = true,
-        },
+
+        config = function()
+            local ts = require("nvim-treesitter")
+
+            local parsers = {
+                "bash",
+                "commonlisp",
+                "cpp",
+                "css",
+                "diff",
+                "gitcommit",
+                "gitignore",
+                "git_config",
+                "git_rebase",
+                "html",
+                "ini",
+                "javascript",
+                "jsdoc",
+                "json",
+                "lua",
+                "luadoc",
+                "luap",
+                "markdown",
+                "markdown_inline",
+                "printf",
+                "python",
+                "query",
+                "rasi",
+                "ssh_config",
+                "tmux",
+                "toml",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "xml",
+                "yaml",
+                "zathurarc",
+            }
+
+            -- Prevents ENTER prompt
+            local function silent_install(list, opts)
+                local old_more = vim.o.more
+                local old_shortmess = vim.o.shortmess
+
+                vim.o.more = false
+                vim.o.shortmess = old_shortmess .. "F"
+
+                ts.install(list, opts)
+
+                vim.o.more = old_more
+                vim.o.shortmess = old_shortmess
+            end
+
+            -- one-time bootstrap marker
+            local marker = vim.fn.stdpath("state") .. "/treesitter_bootstrap_done"
+
+            if vim.fn.filereadable(marker) == 0 then
+                -- FIRST RUN: blocking + silent (bootstrap)
+                silent_install(parsers, { sync = true })
+                vim.fn.writefile({ "done" }, marker)
+            else
+                -- NORMAL RUNS: async (no spam anyway)
+                ts.install(parsers, { sync = false })
+            end
+
+            -- Treesitter setup (main API)
+            ts.setup({
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = {
+                    enable = true,
+                },
+                matchup = {
+                    enable = true,
+                    include_match_words = true,
+                    enable_quotes = true,
+                },
+            })
+
+            -- vim-matchup globals
+            vim.g.matchup_matchparen_offscreen = { method = "popup" }
+            vim.g.matchup_matchparen_deferred = 1
+            vim.g.matchup_matchparen_timeout = 300
+
+            -- attach Treesitter per buffer
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "*",
+                callback = function()
+                    pcall(vim.treesitter.start)
+                    vim.wo.foldmethod = "expr"
+                    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
+            })
+        end,
     },
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
-        vim.treesitter.language.register("bash", "zsh")
-    end,
 }
 
--- Last Modified: Tue, 04 Feb 2025 11:09:34 PM
+-- Last Modified: Sat, 10 Jan 2026 12:38:33 AM
