0da3b9cf8f4e5b7478fe3d704af7b2684d7c3006
cipherodio <cipherodio@gmail.com>
2026-01-10 00:23:50 +0800

    Lsp server configuration

---
+++ .config/nvim/lsp/marksman.lua
@@ -0,0 +1,7 @@
+return {
+    cmd = { "marksman", "server" },
+    filetypes = { "markdown", "markdown.mdx" },
+    root_markers = { ".marksman.toml", ".git" },
+}
+
+-- Last Modified: Tue, 30 Dec 2025 12:27:49 PM
