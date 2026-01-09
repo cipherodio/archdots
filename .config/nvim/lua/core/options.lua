29ed060d3961b2c5d1bf541441e9fe5ea78a0598
cipherodio <cipherodio@gmail.com>
2026-01-10 00:43:07 +0800

    Refactor

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,133 +1,60 @@
 local opt = vim.opt
 local g = vim.g
 
--- g.nomore = true
-opt.background = "dark"
-opt.signcolumn = "yes"
-opt.showbreak = "↪ "
--- opt.listchars = {
---     tab = "│ ",
---     lead = "·",
---     trail = "·",
---     precedes = "«",
---     extends = "»",
--- }
-opt.fillchars = {
-    foldopen = "",
-    foldclose = "",
-    fold = " ",
-    foldsep = " ",
-    diff = "╱",
-    eob = " ",
-}
-opt.laststatus = 3
-opt.spelllang = { "en" }
--- Line numbers
-opt.number = true
-opt.numberwidth = 5
--- Tabs
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.shiftround = true
-opt.expandtab = true
-opt.smartindent = true
--- System clipboard
-opt.clipboard = "unnamedplus"
--- Swap file
-opt.swapfile = false
--- Terminal truecolor support
+-- Miscellaneous
 opt.termguicolors = true
--- Disable mouse support
+opt.background = "dark"
 opt.mouse = ""
--- Disable text wrapping
+opt.updatetime = 200
+opt.fillchars = [[eob: ,fold: ,foldopen:󰅀,foldsep: ,foldclose:󰅂,diff: ]]
+opt.shortmess = "ltToOCFIc"
+opt.clipboard = "unnamedplus"
 opt.wrap = false
--- Don't tabline
-opt.showtabline = 0
--- Search
-opt.smartcase = true
-opt.ignorecase = true
--- Persistent undo
+-- Splits
+opt.splitbelow = true
+opt.splitright = true
+-- Spell
+opt.spelllang = { "en", "tl" }
+opt.spellsuggest = "best,9"
+opt.spelloptions = "camel"
+-- Completion
+-- opt.complete = ".,o"
+opt.completeopt = "menuone,popup,noselect,noinsert,fuzzy"
+opt.pumheight = 10
+opt.wildoptions = "pum,tagfile,fuzzy"
+-- Command line
+opt.cmdheight = 1
+opt.showmode = false -- disables INSERT, VISUAL
+opt.showcmd = false -- disables partial command letters
+-- History
+opt.swapfile = false
 opt.undofile = true
 opt.undolevels = 10000
--- Split
-opt.splitright = true
-opt.splitbelow = true
+-- Cursor
+opt.cursorline = true
+opt.cursorlineopt = "number"
+-- Statuscolumn
+opt.number = true
+opt.numberwidth = 2
+-- Indentation
+opt.expandtab = true
+opt.smartindent = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.shiftround = true
 -- Folds
-opt.foldcolumn = "4"
 opt.foldlevel = 99
 opt.foldlevelstart = 99
--- Scrolloff
-opt.scrolloff = 15
-opt.sidescrolloff = 0
--- Show invisible characters
-opt.list = true
--- Hide markup for bold and italics, but not markers with substitutions
-opt.conceallevel = 2
--- Use ripgrep as grep program
-opt.grepprg = "rg --vimgrep"
-opt.grepformat = "%f:%l:%c:%m"
--- Key timeout to 300ms
-opt.timeoutlen = 300
--- opt.ttimeoutlen = 10
--- Update time
-opt.updatetime = 200
--- Confirm save changed on exit
-opt.confirm = true
--- Keep cursor at the same line when opening a split
-opt.splitkeep = "screen"
--- Window configuration
-opt.winwidth = 5
-opt.winminwidth = 5
-opt.equalalways = false
--- Formatting
-opt.formatoptions = "jcroqlnt"
--- Completion
-opt.completeopt = "menu,menuone,noselect"
-opt.wildmode = "longest:full,full"
-opt.pumheight = 10
--- Allow cursor to move where there is no text in visual block mode
-opt.virtualedit = "block"
--- Partial command letters bellow statusline
-opt.showcmd = false
--- Disable text that shows INSERT, VISUAL
-opt.showmode = false
--- Cmdheight
-opt.cmdheight = 1
--- opt.display:append("uhex")
--- opt.hidden = false
--- Shortmess
-opt.shortmess:append({
-    S = true,
-    s = true,
-    W = true,
-    I = true,
-    c = true,
-    C = true,
-    A = true,
-    F = true,
-})
--- Session save options
-opt.sessionoptions = {
-    "buffers",
-    "curdir",
-    "tabpages",
-    "winsize",
-    "help",
-    "globals",
-    "skiprtp",
-    "folds",
-    "winpos",
-    "resize",
-}
-opt.autowrite = true
+-- opt.foldcolumn = "1"
+opt.foldenable = true
+opt.foldmethod = "indent"
+opt.foldtext = ""
 
--- Disable lsp logging
 vim.lsp.set_log_level(vim.lsp.log_levels.OFF)
 
--- Disabled provider checkhealth warnings
 g.loaded_node_provider = 0
 g.loaded_python3_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
 
--- Last Modified: Fri, 17 Jan 2025 08:49:51 AM
+-- Last Modified: Thu, 08 Jan 2026 06:19:34 PM
