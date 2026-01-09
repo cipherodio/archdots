e8b98eb827c9c1cbcd464a3c80918acdc5688a37
cipherodio <cipherodio@gmail.com>
2026-01-10 00:24:02 +0800

    Lsp server configuration

---
+++ .config/nvim/lsp/ruff.lua
@@ -0,0 +1,13 @@
+return {
+    cmd = { "ruff", "server" },
+    filetypes = { "python" },
+    root_markers = {
+        "pyproject.toml",
+        "ruff.toml",
+        ".ruff.toml",
+        ".git",
+    },
+    settings = {},
+}
+
+-- Last Modified: Tue, 30 Dec 2025 12:38:12 PM
