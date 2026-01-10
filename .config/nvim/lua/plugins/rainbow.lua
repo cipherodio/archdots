636249281e1759e69d971458fae7b6107f7eb602
cipherodio <cipherodio@gmail.com>
2026-01-10 18:43:47 +0800

    Refactor

--- .config/nvim/lua/plugins/rainbow.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "hiphish/rainbow-delimiters.nvim",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile" },
-}
-
--- Last Modified: Mon, 16 Dec 2024 11:06:09 PM
