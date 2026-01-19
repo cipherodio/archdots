e7de9441ed4e662f2bad497b76347fbf964a9987
cipherodio <cipherodio@gmail.com>
2026-01-19 12:57:51 +0800

    Fixed floating window border reverting back to default color

---
+++ .config/qtile/modules/floating.py
@@ -0,0 +1,29 @@
+from libqtile import layout
+from libqtile.config import Match
+from utils.colors import onedark
+
+floating_theme = {
+    "border_width": 3,
+    "border_focus": onedark["c12"],
+    "border_normal": onedark["c01"],
+}
+
+
+floating_layout = layout.Floating(
+    **floating_theme,
+    float_rules=[
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),
+        Match(wm_class="makebranch"),
+        Match(wm_class="maketag"),
+        Match(wm_class="ssh-askpass"),
+        Match(title="branchdialog"),
+        Match(title="pinentry"),
+        Match(wm_class="Steam setup"),
+        Match(title="Steam Settings"),
+        Match(title="Downloading spotify"),
+        Match(wm_class="zenity"),
+    ],
+)
+
+# Last Modified: Sun, 18 Jan 2026 03:04:02 PM
