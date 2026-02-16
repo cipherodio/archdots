f73b71f7b0a7fe262078b7f834fba30af549adcc
cipherodio <cipherodio@gmail.com>
2026-02-16 08:02:15 +0800

    Update firefox user.js

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -6,14 +6,15 @@ user_pref("devtools.toolbox.zoomValue", "1.2");
 // Enable custom userChrome.js:
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
-// Blank Page background color
-// user_pref("browser.display.background_color.dark", "#232326");
 
-// Set homepage to blank page
-user_pref("browser.startup.homepage", "chrome://browser/content/blanktab.html");
-
-// Set New tab to blank page
+// Set Startup to blank page
+user_pref("browser.startup.page", 0);
+// Set Homepage/New Window to blank
+user_pref("browser.startup.homepage", "about:blank");
+// Set New Tab page to blank
 user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.newtab.url", "about:blank");
+
 
 // Disable preloading the New Tab page (saves a bit of memory and avoids any flash of content)
 user_pref("browser.newtab.preload", false);
@@ -57,3 +58,73 @@ user_pref("privacy.clearOnShutdown.cookies", false);
 
 // This could otherwise cause some issues on bank logins and other annoying sites:
 user_pref("network.http.referer.XOriginPolicy", 0);
+
+
+// Enable DRM content (Widevine)
+user_pref("media.eme.enabled", true);
+user_pref("media.gmp-manager.updateEnabled", true);
+user_pref("media.gmp-widevinecdm.enabled", true);
+user_pref("media.gmp-widevinecdm.visible", true);
+
+
+// Disable Recommended extensions as you browse (CFR)
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+// Disable personalized extension recommendations
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+// Disable the "Discover" pane in Add-ons
+user_pref("extensions.htmlaboutaddons.discover.enabled", false);
+// Disable showing the recommendation pane
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.webservice.discoverURL", "");
+user_pref("extensions.getAddons.discovery.api_url", "");
+
+
+// Disable the new Firefox Sidebar revamp
+user_pref("sidebar.revamp", false);
+// Ensure the sidebar is hidden
+user_pref("sidebar.visibility", "hide-sidebar");
+// Optional: If you enabled vertical tabs, disable them
+user_pref("sidebar.verticalTabs", false);
+
+
+// Disable ask to save password
+user_pref("signon.rememberSignons", false);
+// Disable auto filling existing passwords
+user_pref("signon.autofillForms", false);
+// Disable form autocomplete
+user_pref("browser.formfill.enable", false);
+
+
+// Disable saving and filling payment methods
+user_pref("extensions.formautofill.creditCards.available", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("extensions.formautofill.creditCards.save", false);
+// Disable address autofill
+user_pref("extensions.formautofill.addresses.enabled", false);
+// Disable saving addresses
+user_pref("extensions.formautofill.addresses.save", false);
+
+
+// Disable send daily usage ping to Mozilla
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.usage.uploadEnabled", false);
+
+
+// Disable Telemetry
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.server", "data:,");
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+
+// Disable Data Submission
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("datareporting.policy.dataSubmissionPolicyAcceptedVersion", 2);
+
+// Disable personalized extension recommendations
+user_pref("browser.discovery.enabled", false);
