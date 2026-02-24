3a847e901f244233a7ab0065a1561022d52abfce
cipherodio <cipherodio@gmail.com>
2026-02-24 08:06:20 +0800

    Agenda icons

--- .config/nvim/lua/plugins/agenda.lua
+++ .config/nvim/lua/plugins/agenda.lua
@@ -5,9 +5,21 @@ return {
     lazy = false,
     opts = {
         directory = "~/hub/src/mdnotes",
+        -- Scan subdirectories recursively
+        recursive = true,
         -- Options for date_format: "%Y-%m-%d" (2025-12-30),
         --"%m/%d/%Y" (12/30/2025), "%d/%m/%Y" (30/12/2025)
         date_format = "%Y-%m-%d",
+        -- icons = {
+        --     scheduled = "󰐃",
+        --     deadline_urgent = "󱈸", -- ≤1 day
+        --     deadline_soon = "", -- 2-4 days
+        --     deadline_ok = "", -- >4 days
+        --     overdue = "",
+        --     today = "󰁔",
+        --     collapsed = "󰁔",
+        --     expanded = "󰁅",
+        -- },
         keymaps = {
             open = "<leader>aa",
         },
