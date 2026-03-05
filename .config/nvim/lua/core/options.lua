b7f33d40d2865123bbc85dca0ca544e5912828b8
cipherodio <cipherodio@gmail.com>
2026-03-05 19:48:57 +0800

    refactor(options): categorize and organize editor options

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,61 +1,89 @@
 local opt = vim.opt
 local g = vim.g
 
--- General
+-- UI
 opt.termguicolors = true
 opt.background = "dark"
-opt.mouse = ""
-opt.fillchars = [[eob: ,fold: ,foldopen:󰅀,foldsep: ,foldclose:󰅂,diff: ]]
-opt.shortmess = "ltToOCFIc"
-opt.clipboard = "unnamedplus"
-opt.wrap = false
+opt.number = true
+opt.numberwidth = 4
+opt.signcolumn = "no"
+opt.laststatus = 3
+opt.winbar = nil
 opt.scrolloff = 8
 opt.colorcolumn = "80"
-opt.laststatus = 3
-opt.showbreak = "↳"
+opt.fillchars = {
+    eob = " ",
+    diff = " ",
+    vert = "│",
+    horiz = "─",
+}
 
+-- Editor Behavior
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.confirm = true
+opt.virtualedit = "block"
+opt.formatoptions:remove("c", "r", "o")
+opt.formatoptions:append("j", "n", "q")
+opt.splitbelow = true
+opt.splitright = true
+opt.splitkeep = "screen"
+opt.wrap = false
+
+-- Completion
+opt.updatetime = 250
+opt.timeoutlen = 300
+opt.pumheight = 10
+opt.completeopt = {
+    "menuone",
+    "noselect",
+    "noinsert",
+}
+
+-- Command line
 opt.showmode = false
 opt.showcmd = false
+opt.shortmess:append({
+    W = true,
+    F = true,
+    S = true,
+    C = true,
+    I = true,
+    c = true,
+})
 
-opt.updatetime = 50
-opt.completeopt = "menu,menuone,noselect"
-opt.pumheight = 10
-
-opt.number = true
-opt.numberwidth = 2
-opt.signcolumn = "yes"
-
+-- Tabs & Indentation
 opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
-opt.autoindent = true
 opt.smartindent = true
 opt.shiftround = true
 
-opt.foldenable = false
-opt.foldmethod = "manual"
-opt.foldlevel = 99
-
-opt.incsearch = true
+-- Search & Substitution
 opt.inccommand = "split"
 opt.ignorecase = true
 opt.smartcase = true
+opt.grepprg = "rg --vimgrep"
+opt.grepformat = "%f:%l:%c:%m"
 
-opt.swapfile = false
-opt.backup = false
+-- Undo & Persistence
 opt.undofile = true
 opt.undolevels = 10000
+opt.swapfile = false
 
-opt.splitbelow = true
-opt.splitright = true
-
+-- Language & Spelll
 opt.spelllang = { "en", "tl" }
-opt.spellsuggest = "best,9"
+opt.spellsuggest = "best,15"
 opt.spellfile = {
     vim.fn.stdpath("config") .. "/spell/tl.utf-8.add",
     vim.fn.stdpath("config") .. "/spell/en.utf-8.add",
 }
+
+-- Folds
+opt.foldenable = false
+opt.foldmethod = "manual"
+opt.foldlevel = 99
 
 vim.lsp.set_log_level(vim.lsp.log_levels.OFF)
 
