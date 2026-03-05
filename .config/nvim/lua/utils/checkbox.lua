01c279db44db0819e78be1acba0c1abd1c121973
cipherodio <cipherodio@gmail.com>
2026-03-05 19:22:47 +0800

    feat(utils): enhance checkbox markdown module functionality

--- .config/nvim/lua/utils/checkbox.lua
+++ .config/nvim/lua/utils/checkbox.lua
@@ -1,32 +1,32 @@
 local M = {}
 
--- Toggle checkbox state for markdown:
--- [ ] -> [x] -> [-] -> [ ]
+-- Togge Markdown checkbox
+-- Cycle: [ ] -> [x] -> [-] -> [ ]
 function M.toggle()
-    if vim.bo.filetype ~= "markdown" then
-        return
-    end
-
     local line = vim.api.nvim_get_current_line()
     local new_line
-
     if line:find("%[ %]") then
         new_line = line:gsub("%[ %]", "[x]", 1)
-        vim.api.nvim_set_current_line(new_line)
-        return
-    end
-
-    if line:find("%[x%]") then
+    elseif line:find("%[x%]") then
         new_line = line:gsub("%[x%]", "[-]", 1)
-        vim.api.nvim_set_current_line(new_line)
-        return
-    end
-
-    if line:find("%[%-%]") then
+    elseif line:find("%[%-%]") then
         new_line = line:gsub("%[%-%]", "[ ]", 1)
-        vim.api.nvim_set_current_line(new_line)
-        return
+    elseif line:find("^%s*%- ") then
+        new_line = line:gsub("%- ", "- [ ] ", 1)
     end
+    if new_line then
+        vim.api.nvim_set_current_line(new_line)
+    end
+end
+
+-- New checkbox line
+-- Purpose: Smart continuation of task list in both Normal & Insert mode.
+function M.new_checkbox()
+    local line = vim.api.nvim_get_current_line()
+    local indent = line:match("^%s*") or ""
+    local keys =
+        vim.api.nvim_replace_termcodes("<Esc>o" .. indent .. "- [ ] ", true, false, true)
+    vim.api.nvim_feedkeys(keys, "n", true)
 end
 
 return M
