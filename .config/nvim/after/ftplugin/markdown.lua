63c9231f385cc6a6ea9021b8b9ee6c2c7e3e4be1
cipherodio <cipherodio@gmail.com>
2026-03-05 19:59:58 +0800

    refactor(markdown): decouple keymap functions to utils

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,17 +1,33 @@
-local km = require("utils.keys")
+local cb = require("utils.checkbox")
+local h = require("utils.helper")
+local km = require("utils.keyhelper")
+local s = require("utils.scheduler")
 local opt = vim.opt_local
 
 opt.spell = true
 opt.textwidth = 72
 opt.linebreak = true
 
-km.nmap("<leader>tc", function()
-    local cl = vim.api.nvim_get_option_value("conceallevel", { scope = "local" })
+km.nmap("<leader>tc", h.toggle_conceal, { buffer = true, desc = "Toggle conceal" })
 
-    if cl == 0 then
-        vim.api.nvim_set_option_value("conceallevel", 2, { scope = "local" })
-        vim.api.nvim_set_option_value("concealcursor", "", { scope = "local" })
-    else
-        vim.api.nvim_set_option_value("conceallevel", 0, { scope = "local" })
-    end
-end, { desc = "Toggle conceal" })
+-- Markdown agenda scheduler
+km.nmap(
+    "<leader>as",
+    s.insert_scheduled,
+    { buffer = true, desc = "Markdown: Schedule task" }
+)
+km.nmap(
+    "<leader>ad",
+    s.insert_deadline,
+    { buffer = true, desc = "Markdown: Deadline task" }
+)
+km.nmap(
+    "<leader>ab",
+    s.insert_scheduled_and_deadline,
+    { buffer = true, desc = "Markdown: Scheduled + Deadline" }
+)
+-- Markdown checkbox toggle
+km.nmap("<A-CR>", cb.toggle, { buffer = true, desc = "Markdown: Toggle Checkbox" })
+-- Creates new checkbox line
+km.nmap("<S-CR>", cb.new_checkbox, { buffer = true, desc = "Markdown: New checkbox" })
+km.imap("<S-CR>", cb.new_checkbox, { buffer = true, desc = "Markdown: New checkbox" })
