e977e0e20d600e98ce8bfe39bfeba1359d300b2b
cipherodio <cipherodio@gmail.com>
2026-03-05 19:46:03 +0800

    feat(agenda): update to latest API and enable lazy loading

--- .config/nvim/lua/plugins/agenda.lua
+++ .config/nvim/lua/plugins/agenda.lua
@@ -1,27 +1,28 @@
 return {
     "Kamyil/markdown-agenda.nvim",
-    -- Required for :MarkdownAgenda command to be available
-    -- Alternatively, use: cmd = 'MarkdownAgenda' for lazy-loading on command
-    lazy = false,
+    lazy = true,
+    keys = {
+        {
+            "<leader>aa",
+            "<cmd>MarkdownAgenda<cr>",
+            desc = "Open agenda",
+        },
+    },
+    ---@module "markdown-agenda"
     opts = {
         directory = "~/hub/src/mdnotes",
-        -- Scan subdirectories recursively
         recursive = true,
-        -- Options for date_format: "%Y-%m-%d" (2025-12-30),
-        --"%m/%d/%Y" (12/30/2025), "%d/%m/%Y" (30/12/2025)
         date_format = "%Y-%m-%d",
-        -- icons = {
-        --     scheduled = "󰐃",
-        --     deadline_urgent = "󱈸", -- ≤1 day
-        --     deadline_soon = "", -- 2-4 days
-        --     deadline_ok = "", -- >4 days
-        --     overdue = "",
-        --     today = "󰁔",
-        --     collapsed = "󰁔",
-        --     expanded = "󰁅",
-        -- },
-        keymaps = {
-            open = "<leader>aa",
+        help = false,
+        help_separator = false,
+        border = "solid",
+        title = false,
+        calendar = {
+            enabled = true,
+            months_to_show = 3,
+            position = "top",
+            grid_columns = 3,
+            week_start = "monday",
         },
     },
 }
