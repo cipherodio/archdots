a756200306a61078458bfd765fc7551a1948fb10
cipherodio <cipherodio@gmail.com>
2026-03-05 19:33:48 +0800

    fix(neogit): support bare and normal git repositories

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -9,19 +9,63 @@ return {
     keys = {
         {
             "<leader>gg",
-            "<cmd>Neogit<cr>",
-            desc = "Open neogit",
+            function()
+                vim.env.GIT_DIR = nil
+                vim.env.GIT_WORK_TREE = nil
+                require("neogit").open({ cwd = vim.fn.expand("%:p:h") })
+            end,
+            desc = "Open neogit (Project)",
+        },
+        {
+            "<leader>gd",
+            function()
+                vim.env.GIT_DIR = vim.fn.expand("~/.config/.dots")
+                vim.env.GIT_WORK_TREE = vim.fn.expand("~")
+                require("neogit").open({ cwd = vim.fn.expand("~") })
+                -- Optional: Resets after opening so other plugins
+                -- don't get confused
+                -- vim.defer_fn(function()
+                --     vim.env.GIT_DIR = nil
+                --     vim.env.GIT_WORK_TREE = nil
+                -- end, 1000)
+            end,
+            desc = "Open neogit (Dotfiles)",
         },
     },
+    config = function(_, opts)
+        local neogit = require("neogit")
+        neogit.setup(opts)
+
+        -- NOTE: Force a manual refresh after any Git process finishes
+        -- This mimics what happens when you switch Qtile workspaces
+        vim.api.nvim_create_autocmd("User", {
+            pattern = {
+                "NeogitPushComplete",
+                "NeogitPullComplete",
+                "NeogitCommitComplete",
+                "NeogitStageComplete",
+            },
+            callback = function()
+                neogit.refresh()
+            end,
+        })
+    end,
     opts = {
-        console_timeout = 5000,
-        kind = "replace",
-        status = { recent_commit_count = 25 },
+        console_timeout = 10000,
         auto_show_console = false,
-        integrations = {
-            fzf_lua = true,
-            -- diffview = true,
+        auto_refresh = true,
+        refresh_manually = false,
+        kind = "floating",
+        floating = {
+            border = "rounded",
+            width = 0.9,
+            height = 0.8,
         },
+        status = {
+            recent_commit_count = 25,
+            update_on_focus = true,
+        },
+        integrations = { fzf_lua = true },
         mappings = {
             popup = {
                 ["p"] = "PushPopup",
