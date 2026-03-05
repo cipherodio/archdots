e4f8796eb599184e9e90989a39712c48d230cff0
cipherodio <cipherodio@gmail.com>
2026-03-05 19:26:09 +0800

    chore(which-key): refine UI layout and behavior settings

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -2,27 +2,32 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
+        preset = "classic",
         spec = {
             {
                 mode = { "n", "v" },
                 { "<leader>a", group = "action" },
+                { "<leader>e", group = "explore" },
+                { "<leader>w", group = "write/save" },
+                { "<leader>q", group = "quit" },
                 { "<leader>g", group = "git" },
                 { "<leader>f", group = "find" },
                 { "<leader>l", group = "lsp" },
-                { "<leader>m", group = "markdown" },
                 { "<leader>d", group = "debugger" },
                 { "<leader>s", group = "spelling" },
                 { "<leader>t", group = "treesitter" },
-                { "<leader><tab>", group = "buffers" },
+                { "<leader>b", group = "buffer" },
+                { "<leader>y", group = "yank" },
             },
         },
         win = {
-            no_overlap = true,
+            no_overlap = false,
             border = "single",
+            padding = { 1, 2 },
             title = false,
         },
         layout = {
-            spacing = 1,
+            spacing = 3,
             align = "center",
         },
         icons = {
@@ -36,5 +41,12 @@ return {
         },
         show_help = false,
         show_keys = false,
+        delay = 200,
+        plugins = {
+            spelling = {
+                enabled = true,
+                suggestions = 15,
+            },
+        },
     },
 }
