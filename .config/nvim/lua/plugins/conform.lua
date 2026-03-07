5b5995b04655f8f58993ae9c8645f9ce1fa362e3
cipherodio <cipherodio@gmail.com>
2026-03-07 23:22:15 +0800

    feat(conform): swap markdown formatters for rumdl and add xml support

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -19,43 +19,8 @@ return {
             sh = { "shfmt", "shellcheck" },
             zsh = { "shellcheck" },
             python = { "ruff_fix", "ruff_organize_imports", "ruff_format" },
-            markdown = { "markdown-toc", "prettier" },
-        },
-        formatters = {
-            prettier = {
-                -- NOTE: For prettier not working on some directories
-                cwd = function(_, ctx)
-                    local root = vim.fs.find(
-                        { ".git", "package.json", ".prettierrc" },
-                        { upward = true, path = ctx.dirname }
-                    )[1]
-                    return root and vim.fn.fnamemodify(root, ":h") or ctx.dirname
-                end,
-                prepend_args = {
-                    "--config",
-                    vim.fn.expand("~/.config/prettier/.prettierrc"),
-                },
-            },
-            ["markdown-toc"] = {
-                condition = function(_, ctx)
-                    for _, line in
-                        ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false))
-                    do
-                        if line:find("<!%-%- toc %-%->") then
-                            return true
-                        end
-                    end
-                    return false
-                end,
-                args = {
-                    "--indent",
-                    "    ",
-                    "-i",
-                    "$FILENAME",
-                    "--bullets",
-                    "-",
-                },
-            },
+            markdown = { "rumdl" },
+            xml = { "xmlstarlet" },
         },
     },
 }
