ace459a2993753dd8146db98ba1c2926063f8013
cipherodio <cipherodio@gmail.com>
2026-03-14 03:31:42 +0800

    fix(keymap): change highlight colors toggle key from <leader>ac to <leader>ah

--- .config/nvim/lua/plugins/hlcolor.lua
+++ .config/nvim/lua/plugins/hlcolor.lua
@@ -2,7 +2,7 @@ return {
     "brenoprata10/nvim-highlight-colors",
     keys = {
         {
-            "<leader>ac",
+            "<leader>ah",
             function()
                 require("nvim-highlight-colors").toggle()
             end,
