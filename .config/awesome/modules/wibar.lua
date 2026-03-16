243922aabc10271de4045d7a68faf5bab1d2c578
cipherodio <cipherodio@gmail.com>
2026-03-16 17:19:39 +0800

    refactor(wibar): replace hardcoded icons and spacing with helper utilities
    - Use h.create_icon and h.center for consistent icon and text alignment
    - Replace direct imagebox with helper-based window icon
    - Update icon dimensions and remove deprecated id role
    - Consolidate volume and battery widgets into icon+text pairs

--- .config/awesome/modules/wibar.lua
+++ .config/awesome/modules/wibar.lua
@@ -1,6 +1,7 @@
 local awful = require("awful")
 local bindings = require("modules.bindings")
 local gears = require("gears")
+local h = require("utils.helper")
 local wibox = require("wibox")
 local widgets = require("utils.widgets")
 
@@ -31,11 +32,12 @@ M.setup = function(s)
                     {
                         {
                             {
-                                id = "icon_role",
+                                -- id = "icon_role",
+                                image = h.window_icon,
                                 widget = wibox.widget.imagebox,
                             },
-                            height = 20,
-                            width = 20,
+                            height = 24,
+                            width = 24,
                             strategy = "exact",
                             widget = wibox.container.constraint,
                         },
@@ -70,11 +72,10 @@ M.setup = function(s)
             layout = wibox.layout.fixed.horizontal,
             wibox.widget.textbox(" "),
             s.mylayoutbox,
-            wibox.widget.textbox(" 󱑂 "),
-            -- wibox.widget.textclock("%I:%M %p %a %d %b"),
-            wibox.widget.textclock("%I:%M %p"),
-            wibox.widget.textbox("  "),
-            wibox.widget.textclock("%a %d %b"),
+            h.create_icon(h.clock_icon, 10),
+            h.center(wibox.widget.textclock("%I:%M%p")),
+            h.create_icon(h.calendar_icon),
+            h.center(wibox.widget.textclock("%a %d %b")),
             wibox.widget.textbox(" "),
             s.mytaglist,
             wibox.widget.textbox(" "),
@@ -86,15 +87,16 @@ M.setup = function(s)
             layout = wibox.layout.fixed.horizontal,
             wibox.widget.textbox(" "),
             wibox.widget.systray(),
-            widgets.wifi_widget,
-            wibox.widget.textbox(" "),
-            widgets.ram_widget,
-            wibox.widget.textbox(" "),
-            widgets.brightness_widget,
-            wibox.widget.textbox(" "),
-            widgets.volume_widget,
-            wibox.widget.textbox(" "),
-            widgets.bat_widget,
+            h.create_icon(h.wifi_icon),
+            h.center(widgets.wifi_widget),
+            h.create_icon(h.ram_icon),
+            h.center(widgets.ram_widget),
+            h.create_icon(h.brightness_icon),
+            h.center(widgets.brightness_widget),
+            widgets.vol_icon_widget,
+            h.center(widgets.volume_widget),
+            widgets.bat_icon_widget,
+            h.center(widgets.bat_text_widget),
             wibox.widget.textbox(" "),
         },
     })
