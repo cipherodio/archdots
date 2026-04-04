ef2c333e0de5b066ec4217bdcce1a3e4a3be9725
cipherodio <cipherodio@gmail.com>
2026-04-05 00:37:31 +0800

    feat(markdown): add smart snapback and refactor to mdhelper
    - Replace utils.checkbox and utils.helper with utils.mdhelper
    - Add autocmd for smart snapback after hitting textwidth 72
    - Update keymaps to use mdhelper functions for toggle conceal, checkbox, and new checkbox
    - Adjust formatoptions and remove linebreak/wrap settings

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,17 +1,29 @@
-local cb = require("utils.checkbox")
-local h = require("utils.helper")
 local km = require("utils.keyhelper")
+local mh = require("utils.mdhelper")
 local s = require("utils.scheduler")
 local opt = vim.opt_local
 
+local autocmd = vim.api.nvim_create_autocmd
+local function augroup(name)
+    return vim.api.nvim_create_augroup(name, { clear = true })
+end
+
 opt.spell = true
 opt.textwidth = 72
-opt.linebreak = true
-opt.wrap = true
+opt.formatoptions = "tjnq"
+
+autocmd("CursorMovedI", {
+    desc = "Snap back to the left after hitting textwidth 72",
+    group = augroup("snapback"),
+    buffer = 0,
+    callback = function()
+        mh.smart_snap()
+    end,
+})
 
 km.nmap(
     "<leader>mc",
-    h.toggle_conceal,
+    mh.toggle_conceal,
     { buffer = true, desc = "Markdown: toggle conceal" }
 )
 
@@ -31,14 +43,14 @@ km.nmap(
     s.insert_scheduled_and_deadline,
     { buffer = true, desc = "Markdown: scheduled + deadline" }
 )
-km.nmap("<A-CR>", cb.toggle, { buffer = true, desc = "Markdown: toggle checkbox" })
+km.nmap("<A-CR>", mh.toggle, { buffer = true, desc = "Markdown: toggle checkbox" })
 km.nmap(
     "<S-CR>",
-    cb.new_checkbox,
+    mh.new_checkbox,
     { buffer = true, desc = "Markdown: new line checkbox" }
 )
 km.imap(
     "<S-CR>",
-    cb.new_checkbox,
+    mh.new_checkbox,
     { buffer = true, desc = "Markdown: new line checkbox" }
 )
